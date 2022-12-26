6d7082e67b647014b69d9a1c643784380bf43f48
terrianx <txiaopk@gmail.com>
2022-12-26 00:39:05 -0500

    Footer layout changes

--- src/components/main/FooterInfo.vue
+++ src/components/main/FooterInfo.vue
@@ -6,22 +6,68 @@
 
 <template>
   <footer>
-    <a 
-      href="https://terrianx.netlify.app/"
-      target="_blank">
-      Terrian Xiao
-    </a>
+    <div class="links">
+      <a
+        href="#">
+        Holjjang
+      </a>
+      <a
+        href="#">
+        Play
+      </a>
+      <a
+        href="#about">
+        About
+      </a>
+      <a
+        href=""
+        target="_blank">
+        Rules
+      </a>
+      <a
+        href="https://github.com/terrianx/holjjang"
+        target="_blank">
+        Github
+      </a>
+      <a
+        href="https://terrianx.netlify.app/"
+        target="_blank">
+        Terrian Xiao
+      </a>
+    </div>
+    <p class="copyright">© Terrian Xiao 2023. All rights reserved.</p>
   </footer>
 </template>
 
 <style lang="scss" scoped>
+@import '../../assets/Variables.css';
+
   footer {
-    padding: 100px 0;
-    text-align: center;
-    background-color: hsl(32, 100%, 92%);
+    padding: 80px 0 40px 0;
+    display: grid;
+    grid: auto auto / auto;
+    row-gap: 80px;
+    justify-content: center;
+    justify-items: center;
+
+    p {
+      color: var(--dark);
+      font-size: 16px;
+    }
 
     a {
-      font-size: 32px;
+      color: var(--dark);
+      font-size: 24px;
     }
+  }
+
+  .links {
+    display: grid;
+    grid: auto / repeat(6, auto);
+    column-gap: 60px;
+  }
+
+  .copyright {
+    grid-column: span 6;
   }
 </style>
