4cc90a5eaa1f7ffbd7c070aee2e0af8be555127d
terrianx <txiaopk@gmail.com>
2023-01-04 18:09:35 -0500

    Updated about section

--- src/components/main/AboutInfo.vue
+++ src/components/main/AboutInfo.vue
@@ -11,21 +11,22 @@
     <div id="about" class="about">
       <h2>About</h2>
       <p>
-        &emsp;&emsp;&emsp; Odds and evens is a simple game of chance and hand game, 
-        involving two people simultaneously revealing a number 
-        of fingers and winning or losing depending on whether they 
-        are odd or even, or alternatively involving one person 
-        picking up coins or other small objects and hiding them 
-        in their closed hand, while another player guesses whether 
-        they have an odd or even number. The game may be used to make 
-        a decision or played for fun. The finger game is also known 
-        as swords, choosies, pick, odds-on poke, or bucking up.[1] 
-        This zero-sum game, a variation of the ancient morra and 
-        par-impar,[2] is played in Europe, the US, and in Brazil, 
-        especially among children.
+        Holjjang is a two player marble game and a part of Guseulchigi,
+        a marble game played in Korea. Two players compete to obtain all of 
+        their opponents' marbles playing various games. Games vary by region,
+        and some popular ones are Bomdeulgi (throwing marbles into holes), 
+        Alkkagi (throwing marbles to hit other marbles), Byeokchigi (using a
+        wall to play), and Holjjang (odds and evens).
+        <br>
+        <br>
+        To play Guseulchigi, children would make marbles using clay, or
+        play with small objects such as acorns, pebbles, etc. The game began
+        to be played nationwide after ceramic and glass leftovers from manufacturers
+        were used for marbles. Glass production increased especially during
+        the Japanese Occupation.
       </p>
       <a
-        href="https://en.wikipedia.org/wiki/Odds_and_evens_(hand_game)"
+        href="https://folkency.nfm.go.kr/en/topic/detail/1475"
         target="_blank">
         <img src="../../assets/images/outlink-accent.svg" alt="Outlink icon"/>
         Source
